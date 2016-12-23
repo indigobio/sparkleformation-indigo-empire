dc7937056fa2d5a82855bc9e26a209330a9cf7ac
Greg Swallow <gswallow@gmail.com>
2016-12-23 14:33:34 -0600

    Downgrade docker.

--- sparkleformation/components/empire_ami.rb
+++ sparkleformation/components/empire_ami.rb
@@ -1,8 +1,8 @@
 SparkleFormation.component(:empire_ami) do
   mappings(:region_to_ami) do
-    set!('us-east-1'.disable_camel!, :ami => 'ami-ffdcd5e8')
-    set!('us-east-2'.disable_camel!, :ami => 'ami-89cb91ec')
-    set!('us-west-1'.disable_camel!, :ami => 'ami-77d68017')
-    set!('us-west-2'.disable_camel!, :ami => 'ami-5b51e43b')
+    set!('us-east-1'.disable_camel!, :ami => 'ami-cbe6f0dc')
+    set!('us-east-2'.disable_camel!, :ami => 'ami-88c59fed')
+    set!('us-west-1'.disable_camel!, :ami => 'ami-9f6d3cff')
+    set!('us-west-2'.disable_camel!, :ami => 'ami-694ff809')
   end
 end
