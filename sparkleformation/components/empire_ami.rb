ecf581fd6f7d6c9f230c58bcefbd39c645ba786c
Greg Swallow <gswallow@gmail.com>
2016-12-29 12:35:51 -0500

    Update AMI.

--- sparkleformation/components/empire_ami.rb
+++ sparkleformation/components/empire_ami.rb
@@ -1,8 +1,8 @@
 SparkleFormation.component(:empire_ami) do
   mappings(:region_to_ami) do
-    set!('us-east-1'.disable_camel!, :ami => 'ami-cbe6f0dc')
-    set!('us-east-2'.disable_camel!, :ami => 'ami-88c59fed')
-    set!('us-west-1'.disable_camel!, :ami => 'ami-9f6d3cff')
-    set!('us-west-2'.disable_camel!, :ami => 'ami-694ff809')
+    set!('us-east-1'.disable_camel!, :ami => 'ami-e16b77f6')
+    set!('us-east-2'.disable_camel!, :ami => 'ami-fef4ae9b')
+    set!('us-west-1'.disable_camel!, :ami => 'ami-6decbd0d')
+    set!('us-west-2'.disable_camel!, :ami => 'ami-16d56476')
   end
 end
