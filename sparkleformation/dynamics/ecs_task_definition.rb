62e696c5b75a6ba268f14d0f0c9acec30624ceb2
Greg Swallow <gswallow@gmail.com>
2017-01-30 16:09:34 -0500

    Add support for IAM roles for ECS tasks.

--- sparkleformation/dynamics/ecs_task_definition.rb
+++ sparkleformation/dynamics/ecs_task_definition.rb
@@ -1,5 +1,8 @@
 SparkleFormation.dynamic(:task_definition) do |_name, _config = {}|
   dynamic!(:ecs_task_definition, _name).properties do
+    if _config.has_key?(:task_role)
+      task_role_arn attr!(_config[:task_role], :arn)
+    end
     container_definitions _array(
       *_config[:container_definitions].map { |cd| registry!(:container_definition, cd) }
     )
