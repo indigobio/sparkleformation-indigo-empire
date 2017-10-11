4e22aea1f0ee049bf357e5207b0a5572df3f7ca1
Greg Swallow <gswallow@gmail.com>
2017-10-11 15:26:20 -0400

    Wait for emp apps to delete; reuse del_stack function.

--- scripts/teardown.sh
+++ scripts/teardown.sh
@@ -14,11 +14,33 @@ run_if_yes () {
   esac
 }
 
+del_stack () {
+  stack=$1
+  run_if_yes "aws cloudformation delete-stack --stack-name $stack"
+  echo -n "Waiting for resource deletion"
+  while [ "$(aws cloudformation describe-stacks --stack-name $stack --query 'Stacks[].StackStatus' --output text)" == "DELETE_IN_PROGRESS" ]; do
+    echo -n .
+    sleep 1
+  done
+  if [ "$(aws cloudformation describe-stacks --stack-name $stack --query 'Stacks[].StackStatus' --output text)" == "DELETE_FAILED" ]; then
+    echo "$stack failed to delete"
+    exit 1
+  fi
+}
+
 # Tear down Empire apps
 emp apps > /dev/null 2>&1 && : || emp login
 for i in $(emp apps | awk '{print $1}') ; do
   run_if_yes "echo $i | emp destroy $i"
 done
+
+# Wait for apps to delete
+echo -n "Waiting for emp apps to delete"
+while [ "$(aws cloudformation list-stacks --stack-status-filter DELETE_IN_PROGRESS --query 'StackSummaries[] | length(@)')" -gt 0 ]; do
+  echo -n .
+  sleep 1
+done
+echo
 
 # Tear down the custom resources S3 bucket
 for bucket in $(aws s3api list-buckets --query 'Buckets[?contains(Name, `customresources`) == `true`].Name' --output text) ; do
@@ -32,4 +54,4 @@ stack=$(aws cloudformation list-stacks --stack-status-filter CREATE_COMPLETE UPD
   --query 'StackSummaries[].StackId' --output table | grep ${environment}-empire-${AWS_DEFAULT_REGION} \
   | awk '{print $2}')
 
-run_if_yes "aws cloudformation delete-stack --stack-name $stack"
+del_stack $stack
